20c21b06e95062fa9e06aea7dc89c310ccce644f
bkonkle <brandon@konkle.us>
2014-07-27 22:25:33 -0600

    Tweak to fix HTML structure

--- src/components/ListComponent.js
+++ src/components/ListComponent.js
@@ -1,5 +1,6 @@
 (function () {
 
+  var div = React.DOM.div;
   var ul = React.DOM.ul;
   var li = React.DOM.li;
   var input = React.DOM.input;
@@ -15,12 +16,13 @@
     },
 
     render: function() {
-      return ul({}, this.state.items.map(function(item) {
-        return li({}, item);
-      }, this).concat([
+      return div({}, [
+        ul({}, this.state.items.map(function(item) {
+          return li({}, item);
+        }, this)),
         input({'type': 'text', 'ref': 'itemInput'}),
         button({'onClick': this.onClick}, 'Add')
-      ]));
+      ]);
     },
 
     onClick: function() {
